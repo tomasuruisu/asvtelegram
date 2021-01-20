3eb3d4ce0d07c6465c7a33dacb95f30e1f98d34b
thomas <thomaslewis94@gmail.com>
2021-01-20 18:11:22 +0100

    updated unit tests according to AAA and CORRECT

--- TMessagesProj/src/test/java/MessengerTest.java
+++ TMessagesProj/src/test/java/MessengerTest.java
@@ -22,56 +22,128 @@ import static org.junit.Assert.assertThat;
 
 public class MessengerTest {
 
+    /*
+     * Are the values CORRECT?
+     * Yes, all values conform to the expected format.
+     * All values are within a reasonable range: true/false,
+     * no empty strings or 0-values (unless intended) and there are no null-values passed on.
+     * There is also no reference to anything external that could influence it.
+     * No unnecessary values are instantiated.
+     * Everything is done in the correct order in a synchronous manner.
+     */
     @Test
     public void messageSent() {
+        // Arrange
         final int MESSAGE_SEND_STATE_SENT = 0;
+        boolean messageSent;
         TLRPC.TL_message newMsg = new TLRPC.TL_message();
         newMsg.send_state = MESSAGE_SEND_STATE_SENT;
         MessageObject messageObject = new MessageObject(1, newMsg,
                 "message", "messageName", "userName",
                 false, false, false, false);
 
-        assertTrue(messageObject.isSent());
+        // Act
+        messageSent = messageObject.isSent();
+
+        // Assert
+        assertTrue(messageSent);
     }
 
+    /*
+     * Are the values CORRECT?
+     * Yes, all values conform to the expected format.
+     * All values are within a reasonable range: true/false,
+     * no empty strings or 0-values (unless intended) and there are no null-values passed on.
+     * There is also no reference to anything external that could influence it.
+     * No unnecessary values are instantiated.
+     * Everything is done in the correct order in a synchronous manner.
+     */
     @Test
     public void messageIsPhoto() {
+        // Arrange
+        boolean messageIsPhoto;
         TLRPC.TL_message newMsg = new TLRPC.TL_message();
         newMsg.media = new TLRPC.TL_messageMediaPhoto();
 
-        assertTrue(MessageObject.isPhoto(newMsg));
+        // Act
+        messageIsPhoto = MessageObject.isPhoto(newMsg);
+
+        // Assert
+        assertTrue(messageIsPhoto);
     }
 
+    /*
+     * Are the values CORRECT?
+     * Yes, all values conform to the expected format.
+     * All values are within a reasonable range: true/false,
+     * no empty strings or 0-values (unless intended) and there are no null-values passed on.
+     * There is also no reference to anything external that could influence it.
+     * No unnecessary values are instantiated.
+     * Everything is done in the correct order in a synchronous manner.
+     */
     @Test
     public void hasGroupChatAdminRights() {
+        // Arrange
+        boolean hasGroupChatAdminRights;
         TLRPC.TL_chat tl_chat = new TLRPC.TL_chat();
         tl_chat.creator = true;
 
-        assertTrue(ChatObject.hasAdminRights(tl_chat));
+        // Act
+        hasGroupChatAdminRights = ChatObject.hasAdminRights(tl_chat);
+
+        // Assert
+        assertTrue(hasGroupChatAdminRights);
     }
 
+    /*
+     * Are the values CORRECT?
+     * Yes, all values conform to the expected format.
+     * All values are within a reasonable range: true/false,
+     * no empty strings or 0-values (unless intended) and there are no null-values passed on.
+     * There is also no reference to anything external that could influence it.
+     * No unnecessary values are instantiated.
+     * Everything is done in the correct order in a synchronous manner.
+     */
     @Test
     public void ensurePhotoMessageIsSecret() {
+        // Arrange
+        boolean photoMessageIsSecret;
         TLRPC.TL_message_secret tl_message_secret = new TLRPC.TL_message_secret();
         tl_message_secret.media = new TLRPC.TL_messageMediaPhoto();
         tl_message_secret.ttl = 1;
         MessageObject messageObject = new MessageObject(1, tl_message_secret,
                 "secret message", "messageName", "userName",
-                false, false, false, true);
+                false, false,false, true);
 
-        assertTrue(messageObject.shouldEncryptPhotoOrVideo()
-                && messageObject.needDrawBluredPreview() && messageObject.isSecretMedia());
+        // Act
+        photoMessageIsSecret = messageObject.shouldEncryptPhotoOrVideo()
+                && messageObject.needDrawBluredPreview() && messageObject.isSecretMedia();
+
+        // Assert
+        assertTrue(photoMessageIsSecret);
     }
 
+    /*
+     * Are the values CORRECT?
+     * Yes, all values conform to the expected format.
+     * All values are within a reasonable range: true/false,
+     * no empty strings or 0-values (unless intended) and there are no null-values passed on.
+     * There is also no reference to anything external that could influence it.
+     * No unnecessary values are instantiated.
+     * Everything is done in the correct order in a synchronous manner.
+     */
     @Test
     public void setUserPhoneNumber() {
+        // Arrange
         String phoneNumber = "0612345678";
         TLRPC.User user = new TLRPC.TL_user();
         user.phone = phoneNumber;
         UserConfig userConfig = new UserConfig(1);
 
+        // Act
         userConfig.setCurrentUser(user);
 
+        // Assert
         assertSame(userConfig.getClientPhone(), phoneNumber);
     }
 }
